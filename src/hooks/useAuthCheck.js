727e7fa392a02f493c4d53b738aa1fc0c7356596
Your Name <you@example.com>
2026-05-03 01:21:36 +0300

    add jwt handling

--- src/hooks/useAuthCheck.js
+++ src/hooks/useAuthCheck.js
@@ -11,7 +11,7 @@ export default function useAuthCheck(allowedRoles) {
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('rag-jwt');
     if (!token) {
       navigate('/login', { replace: true });
       return;
